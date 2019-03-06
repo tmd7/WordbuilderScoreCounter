79428649b616c3b818657e44036fa9ff5f9015ce
mtlepberghenov <mur.marat@gmail.com>
2019-03-06 12:25:15 +0200

    Fix: Added some provide methods and added a name parameter of the App class

--- app/src/main/java/com/mtlepberghenov/wordbuilderscorecounter/application/di/WordbuilderScoreCounterModule.kt
+++ app/src/main/java/com/mtlepberghenov/wordbuilderscorecounter/application/di/WordbuilderScoreCounterModule.kt
@@ -2,6 +2,8 @@ package com.mtlepberghenov.wordbuilderscorecounter.application.di
 
 import android.content.Context
 import com.mtlepberghenov.wordbuilderscorecounter.base.BaseApplication
+import com.mtlepberghenov.wordbuilderscorecounter.coroutines.CoroutinesContextProvider
+import com.mtlepberghenov.wordbuilderscorecounter.coroutines.DefaultCoroutinesContextProvider
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -14,4 +16,9 @@ class WordbuilderScoreCounterModule(val application: BaseApplication) {
     @Provides
     @Singleton
     fun provideAppContext(): Context = application
+
+    @Provides
+    @Singleton
+    fun provideCoroutinesContextProvider(): CoroutinesContextProvider =
+            DefaultCoroutinesContextProvider()
 }
--- app/src/main/java/com/mtlepberghenov/wordbuilderscorecounter/screens/home/di/HomeModule.kt
+++ app/src/main/java/com/mtlepberghenov/wordbuilderscorecounter/screens/home/di/HomeModule.kt
@@ -2,22 +2,27 @@ package com.mtlepberghenov.wordbuilderscorecounter.screens.home.di
 
 import com.mtlepberghenov.wordbuilderscorecounter.base.BasePresenter
 import com.mtlepberghenov.wordbuilderscorecounter.coroutines.CoroutinesContextProvider
-import com.mtlepberghenov.wordbuilderscorecounter.screens.home.HomeInteractor
-import com.mtlepberghenov.wordbuilderscorecounter.screens.home.HomePresenter
-import com.mtlepberghenov.wordbuilderscorecounter.screens.home.HomeRouter
-import com.mtlepberghenov.wordbuilderscorecounter.screens.home.HomeView
+import com.mtlepberghenov.wordbuilderscorecounter.screens.home.*
+import dagger.Module
 import dagger.Provides
 
+@Module
 class HomeModule {
     @Provides
     fun providePresenter(
         router: HomeRouter,
         interactor: HomeInteractor,
         coroutinesContextProvider: CoroutinesContextProvider
-    ) : BasePresenter<HomeView, HomeRouter, HomeInteractor> =
+    ): BasePresenter<HomeView, HomeRouter, HomeInteractor> =
         HomePresenter(
             router,
             interactor,
             coroutinesContextProvider
         )
+
+    @Provides
+    fun provideHomeRouter(activity: HomeActivity): HomeRouter = DefaultHomeRouter(activity)
+
+    @Provides
+    fun provideHomeInteractor(): HomeInteractor = DefaultHomeInteractor()
 }
