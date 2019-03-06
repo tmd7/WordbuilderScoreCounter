54e0147e082491d5af2cb98383765fae3cb0dc46
mtlepberghenov <mur.marat@gmail.com>
2019-03-06 10:54:35 +0200

    Created Base Presenter with base logic

--- app/src/main/java/com/mtlepberghenov/wordbuilderscorecounter/base/BasePresenter.kt
+++ app/src/main/java/com/mtlepberghenov/wordbuilderscorecounter/base/BasePresenter.kt
@@ -1,2 +1,35 @@
 package com.mtlepberghenov.wordbuilderscorecounter.base
 
+import com.mtlepberghenov.wordbuilderscorecounter.coroutines.CoroutinesContextProvider
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+
+abstract class BasePresenter<V : BaseView, R : Router, I : Interactor>(
+    protected val router: R,
+    protected val interactor: I,
+    coroutinesContextProvider: CoroutinesContextProvider
+) : CoroutineScope {
+    protected var job = Job()
+    protected val IOContext = coroutinesContextProvider.IOContext
+    override val coroutineContext = coroutinesContextProvider.IOContext + job
+
+    protected lateinit var view: V
+
+    fun takeView(view: V) {
+        this.view = view
+        initJobIfNeeded()
+        onCreate()
+    }
+
+    abstract fun onCreate()
+
+    private fun initJobIfNeeded() {
+        if (job.isCancelled) {
+            job = Job()
+        }
+    }
+
+    open fun onDestroy() {
+        job.cancel()
+    }
+}
