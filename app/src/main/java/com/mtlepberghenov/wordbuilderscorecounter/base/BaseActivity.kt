535a7c21ca2b23110a9ed8aa44fabb7085cd7536
mtlepberghenov <mur.marat@gmail.com>
2019-03-06 10:51:46 +0200

    Create Base activity with super methods

--- app/src/main/java/com/mtlepberghenov/wordbuilderscorecounter/base/BaseActivity.kt
+++ app/src/main/java/com/mtlepberghenov/wordbuilderscorecounter/base/BaseActivity.kt
@@ -1,2 +1,24 @@
 package com.mtlepberghenov.wordbuilderscorecounter.base
 
+import android.os.Bundle
+import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
+
+abstract class BaseActivity<V : BaseView, R : Router, I : Interactor> : DaggerAppCompatActivity() {
+    abstract val resId: Int
+    abstract val view: V
+
+    @Inject
+    lateinit var presenter: BasePresenter<V, R, I>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(resId)
+        presenter.takeView(view)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.onDestroy()
+    }
+}
